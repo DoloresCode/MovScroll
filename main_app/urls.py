b6981233cae4c1bf0a260f8a6625351ce6e2e00f
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-01 18:06:42 -0400

    url for artist as well as view created and list of artists too

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), 
-    path('about/', views.About.as_view(), name='about'), 
+    path('about/', views.About.as_view(), name='about'),
+    path('actors/', views.ActorList, name='actor_list'), 
 ]
