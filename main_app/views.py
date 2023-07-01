bdcecb0b3bcb19a4d33a01508c4fc3b947e0d05c
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-01 15:26:38 -0400

    view defined

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View # <- View class to handle request
+from django.http import HttpResponse 
 
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse("MoveScroll")
