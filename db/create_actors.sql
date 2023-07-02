01dd69f3135c1d014757e56ba8eb408ba10b086a
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-01 22:16:35 -0400

    actot list add and all bug clean in db

---
+++ db/create_actors.sql
@@ -0,0 +1,143 @@
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Kevin Costner', 'https://ntvb.tmsimg.com/assets/assets/36838_v9_bc.jpg?w=270&h=360', 'Kevin Costner is an American actor and filmmaker. He has received various accolades, including two Academy Awards, three Golden Globe Awards, a Primetime Emmy Award, and two Screen Actors Guild Awards.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Jennifer Grey', 'https://www.thesun.co.uk/wp-content/uploads/2017/08/nintchdbpict000005058542.jpg', 'Jennifer Grey is an American actress, known for her roles in the 1980s films Ferris Bueller s Day Off and Dirty Dancing.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Irene Cara', 'https://www.thesun.co.uk/wp-content/uploads/2022/11/street-tale-hollywood-premiere-laemmle-778439188.jpg', 'Irene Cara is an American singer, songwriter, dancer, and actress, known for her roles in Fame.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Sigourney Weaver', 'https://m.media-amazon.com/images/M/MV5BMTk1MTcyNTE3OV5BMl5BanBnXkFtZTcwMTA0MTMyMw@@._V1_.jpg', 'Sigourney Weaver is an American actress. Dubbed "The Sci-Fi Queen", Weaver is considered to be a pioneer of action heroines in science fiction films.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Harrison Ford', 'https://upload.wikimedia.org/wikipedia/commons/3/34/Harrison_Ford_by_Gage_Skidmore_3.jpg', 'Harrison Ford is an American actor, pilot, and environmental activist. He gained worldwide fame for his starring role as Han Solo in the Star Wars franchise and as the title character of the Indiana Jones film series.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Bill Murray', 'https://flxt.tmsimg.com/assets/8327_v9_bc.jpg', 'Bill Murray is an American actor, comedian, and writer. Known for his deadpan delivery, he first rose to fame on Saturday Night Live, a series of performances that earned him his first Emmy Award.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Eddie Murphy', 'https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkwNTgxMDM0Nzk5ODAyMjM2/eddie-murphy-ftr.jpg', 'Eddie Murphy is an American actor, comedian, writer, producer, and singer. He was a regular cast member on Saturday Night Live from 1980 to 1984. He has worked as a stand-up comedian and was ranked No. 10 on the list of Comedy Centralregarding the 100 Greatest Stand-ups of All Time.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Wil Wheaton', 'https://yt3.googleusercontent.com/ytc/AGIKgqPDxvFqPmpGyN8PdLwK2zde7OKxwoeQ9D0JIKnToEI=s900-c-k-c0x00ffffff-no-rj', 'Wil Wheaton is an American actor, blogger, and writer. He portrayed Wesley Crusher on the television series Star Trek: The Next Generation, Gordie Lachance in the film Stand by Me, Joey Trotta in Toy Soldiers, and Bennett Hoenicker in Flubber.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Tom Hanks', 'https://flxt.tmsimg.com/assets/62982_v9_bb.jpg', 'Tom Hanks is an American actor and filmmaker. Known for both his comedic and dramatic roles, he is one of the most popular and recognizable film stars worldwide, and is regarded as an American cultural icon.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Sandra Bullock', 'https://www.emdria.org/wp-content/uploads/2021/12/Sandra-Bullock-Shutterstock-scaled.jpg', 'Sandra Bullock is an American actress and producer. The recipient of various accolades, including an Academy Award and a Golden Globe Award, she was the actress in the world the highest-paid in both 2010 and 2014.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Shia LaBeouf', 'https://pyxis.nymag.com/v1/imgs/532/b98/d38941d9d53cc5fbb60f760a6b90cc8287-shia-labeouf.rsquare.w330.jpg', 'Shia LaBeouf is an American actor, performance artist, and filmmaker. He played Louis Stevens in the Disney Channel series Even Stevens, a role for which he received a Young Artist Award nomination in 2001 and won a Daytime Emmy Award in 2003.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Bruce Willis', 'https://m.media-amazon.com/images/M/MV5BMjA0MjMzMTE5OF5BMl5BanBnXkFtZTcwMzQ2ODE3Mw@@._V1_FMjpg_UX1000_.jpg', 'Bruce Willis is an American actor and film producer. His career began on the Off-Broadway stage in the 1970s.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Rami Malek', 'https://flxt.tmsimg.com/assets/330277_v9_bb.jpg', 'Rami Malek is an American actor. He is known for his lead role as Elliot Alderson in the television series Mr. Robot, for which he received the Primetime Emmy Award for Outstanding Lead Actor in a Drama Series.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Daniel Brühl', 'https://www.themoviedb.org/t/p/w500/3YlmTfiy5qZXkrdKGjaM1uMjGKP.jpg', 'Daniel Brühl is a Spanish-German actor. He began his work at a young age in a German soap opera called Forbidden Love in 1995.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Marilyn Monroe', 'https://hips.hearstapps.com/hmg-prod/images/news-photo-515346396-1562946515.jpg', 'Marilyn Monroe was an American actress, model, and singer. Famous for playing comedic "blonde bombshell" characters, she became one of the most popular sex symbols of the 1950s and early 1960s and was emblematic of the era s changing attitudes towards sexuality. Marilyn Monroe, beyond her acting career and iconic status, was notably intelligent. She owned a personal library of over 400 books, encompassing a range of classics, modern literature, and works on history and psychology, demonstrating her broad intellectual interests. Despite her limited formal education, she pursued knowledge independently. In addition, she demonstrated business acumen by founding her own production company in 1955, becoming one of the first women in Hollywood to do so.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES 
+        ('Whitney Houston', 'https://cdn.smehost.net/whitneyhoustoncom-uslegacyprod/wp-content/uploads/2018/07/150116_randee_edit.jpg', 'Whitney Houston was an American singer and actress. She is one of the best-selling recording artists of all time, with sales of over 200 million records worldwide. Houston released seven studio albums and two soundtrack albums, all of which have been certified diamond, multi-platinum, platinum, or gold by the Recording Industry Association of America (RIAA).', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Patrick Swayze', 'https://i.pinimg.com/originals/99/69/ea/9969eace493fb41633c9b9d4226a53ab.jpg', 'Patrick Swayze was an American actor, dancer, and singer-songwriter. He became well-known during the mid-1980s after starring in the box office success the movie Dirty Dancing and later for his role in the movie Ghost, which was another major box office hit.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Michael Biehn', 'https://thumbs.dreamstime.com/b/michael-biehn-24037131.jpg', 'Michael Biehn is an American actor, primarily known for his military roles in science fiction films directed by James Cameron; as Sgt. Kyle Reese in the movie The Terminator , Cpl. Dwayne Hicks in the movie Aliens, and Lt. Coffey in the movie The Abyss.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Karen Allen', 'https://upload.suggest.com/sg/uploads/2021/01/karen-allen-at-the-independen3b5802-702x526.jpg', 'Karen Allen is an American film and stage actress. After making her film debut in the movie Animal House, she became best known for her role as Marion Ravenwood in the movie Raiders of the Lost Ark and Indiana Jones and the Kingdom of the Crystal Skull.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Dan Aykroyd', 'https://m.media-amazon.com/images/M/MV5BMTI2MDA3NTg0NF5BMl5BanBnXkFtZTYwMzM5ODgz._V1_FMjpg_UX1000_.jpg', 'Dan Aykroyd is a Canadian actor, producer, comedian, musician, and filmmaker who was an original member of the the movie Not Ready for Prime Time Players on Saturday Night Live. He co-starred in comedy hits such as the movie The Blues Brothers and Ghostbusters.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('River Phoenix', 'https://www.notrecinema.com/images/usercontent/star/river-phoenix-photo_45975_31233.jpg', 'River Phoenix was an influential American actor and musician who stood out for his compelling performances and depth of character. Making his mark as a child actor, Phoenix effortlessly transitioned to more mature roles, captivating audiences with his poignant performances in films like Stand by Me and My Own Private Idaho. Notable for his profound emotional depth and commitment, Phoenix was renowned for immersing himself fully in his roles. His talent and dedication earned him an Academy Award nomination for his work in Running on Empty, and he remains an icon of his generation, celebrated for his immense contribution to the film industry.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Dan Aykroyd', 'https://m.media-amazon.com/images/M/MV5BMTI2MDA3NTg0NF5BMl5BanBnXkFtZTYwMzM5ODgz._V1_FMjpg_UX1000_.jpg', 'Dan Aykroyd is a Canadian actor, producer, comedian, musician, and filmmaker who was an original member of the the movie Not Ready for Prime Time Players on Saturday Night Live. He co-starred in comedy hits such as the movie The Blues Brothers and Ghostbusters.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Drew Barrymore', 'https://c4.wallpaperflare.com/wallpaper/386/135/650/women-actress-blonde-short-hair-wallpaper-preview.jpg', 'Drew Barrymore is an American actress, producer, director, talk show host and entrepreneur. She is the recipient of numerous accolades, including a Golden Globe, a Screen Actors Guild Award, and a BAFTA nomination. She gained fame as a child actress with her role in E.T. the Extra-Terrestrial and is also known for her roles in The Wedding Singer and Charlie''s Angels.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Whoopi Goldberg', 'https://iv1.lisimg.com/image/7937314/600full-whoopi-goldberg.jpg', 'Whoopi Goldberg is an American actress, comedian, author, and television personality. She is one of sixteen entertainers to have won an Emmy Award, a Grammy Award, an Academy Award, and a Tony Award (EGOT). She is also known for her work in film such as The Color Purple and Ghost, for which she won the Academy Award for Best Supporting Actress.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Oprah Winfrey', 'https://cdn2.kennedy-center.org/images/slideshow/H100903_OprahWinfrey_LG.jpg', 'Oprah Winfrey is an American talk show host, television producer, actress, author, and philanthropist. She is best known for her talk show, The Oprah Winfrey Show, broadcast from Chicago, which was the highest-rated television program of its kind in history and ran in national syndication for 25 years from 1986 to 2011.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Danny Glover', 'https://i.mydramalist.com/WwZpR_5c.jpg', 'Danny Glover is an American actor, film director, and political activist. He is widely known for his lead role as Roger Murtaugh in the Lethal Weapon film series. He also has leading roles in the films The Color Purple, To Sleep with Anger, and Predator 2.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Robin Wright', 'https://flxt.tmsimg.com/assets/55876_v9_bc.jpg', 'Robin Wright is an American actress and director. She is the recipient of eight Primetime Emmy Award nominations and has earned a Golden Globe Award and a Satellite Award for her work in television. Wright first gained attention for her role in the NBC Daytime soap opera Santa Barbara, as Kelly Capwell from 1984 to 1988. She then made the transition to film, starring in the romantic comedy fantasy adventure film The Princess Bride in 1987.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Quinton Aaron', 'https://m.media-amazon.com/images/M/MV5BODQ3Y2JhZGYtY2I3Yy00M2Y3LWE3YzYtYWZiMDI4YjQwMzRmXkEyXkFqcGdeQXVyNDUzNjIxMzg@._V1_.jpg', 'Quinton Aaron is an American actor whose first lead role was as Michael Oher in the 2009 film The Blind Side.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Megan Fox', 'https://www.instyle.com/thmb/TlA5NSmLfbOOa8Z7tHvnbpFw4SI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/101515-megan-fox-lead-b7fa6026bcd746bd87ae520a617d25e2.jpg', 'Megan Fox is an American actress and model. She made her acting debut in the family film Holiday in the Sun. This was followed by numerous supporting roles in film and television, as well as a starring role in the ABC sitcom Hope & Faith. Fox went on to make her feature film debut in the teen musical comedy Confessions of a Teenage Drama Queen. She rose to fame with her breakout role in the blockbuster action film Transformers.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Samuel L. Jackson', 'https://static.wikia.nocookie.net/unbreakablemovie/images/2/22/Samuel-L-Jackson-actor-858942.jpg/revision/latest?cb=20170820171318', 'Samuel L. Jackson is an American actor and producer. Widely regarded as one of the most popular actors of his generation, the films in which he has appeared have collectively grossed over $27 billion worldwide, making him the highest-grossing actor of all time. He rose to fame with films such as Pulp Fiction, Die Hard with a Vengeance, and the Star Wars prequel trilogy.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Lucy Boynton', 'https://c4.wallpaperflare.com/wallpaper/151/766/449/lucy-boynton-women-actress-green-eyes-british-hd-wallpaper-preview.jpg', 'Lucy Boynton is an American-English actress. She made her film debut with a leading role in Miss Potter. Boynton''s breakthrough came in 2018 when she portrayed Mary Austin in the biographical film Bohemian Rhapsody.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Chris Hemsworth', 'https://cdn.britannica.com/92/215392-050-96A4BC1D/Australian-actor-Chris-Hemsworth-2019.jpg', 'Chris Hemsworth is an Australian actor. He rose to prominence playing Kim Hyde in the Australian television series Home and Away before beginning a film career in Hollywood. Hemsworth is best known for playing Thor in the Marvel Cinematic Universe, beginning with Thor in 2011 and appearing most recently in What If...? in 2021, which established him among the world''s highest-paid actors.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Olivia Wilde', 'https://images.mubicdn.net/images/cast_member/51059/cache-702635-1628161655/image-w856.jpg?size=800x', 'Olivia Wilde is an American actress and filmmaker. She is known for her role as Remy ''Thirteen'' Hadley on the medical-drama television series House, and her roles in the films Tron: Legacy, Cowboys & Aliens, The Incredible Burt Wonderstone, and The Lazarus Effect.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at) 
+    VALUES
+        ('Tony Curtis', 'https://static.onecms.io/wp-content/uploads/sites/6/2010/09/tony-curtis_l.jpg', 'Tony Curtis was an American film actor whose career spanned six decades. He acted in more than 100 films in roles covering a wide range of genres, from light comedy to serious drama. His notable films include Houdini, Spartacus, and Some Like It Hot.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Jack Lemmon', 'https://images.fineartamerica.com/images-medium-large/jack-lemmon-portrait-for-an-all-star-everett.jpg', 'Jack Lemmon was an American actor and musician who was nominated for an Academy Award eight times, winning twice. He starred in over 60 films, including Some Like It Hot, The Apartment, Mister Roberts, Days of Wine and Roses, The Great Race, Irma la Douce, The Odd Couple, Save the Tiger, The Out-of-Towners, The China Syndrome, Missing, Glengarry Glen Ross, Tuesdays with Morrie, and Grumpy Old Men.', 't', '2023-07-01 00:00:00.000000-07');
+
+INSERT INTO main_app_actor(name, img, bio, verified_actor, created_at)
+    VALUES
+        ('Paul Bates', 'https://i.pinimg.com/564x/a6/f1/9b/a6f19b6546925b4b8f52eef06f5c8ce8.jpg', 'Paul Bates is an American actor, known for his roles in the movie Coming to America, 8 Mile, and its sequel, the movie Coming 2 America.', 't', '2023-07-01 00:00:00.000000-07');
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import View # <- View class to handle request
+from django.views import View
 from django.http import HttpResponse 
 from django.views.generic.base import TemplateView
 
