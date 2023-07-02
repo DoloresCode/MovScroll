4c4046db3c61ed937332d771f653c6e7238fb3b8
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-01 20:08:38 -0400

    model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Actor(models.Model):
+
+    name = models.CharField(max_length=400)
+    img = models.CharField(max_length=700)
+    bio = models.TextField(max_length=3000)
+    verified_actor = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
