4f13d0527aeb504ee1c9c1c63da7f6513ee8c1d7
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-02 23:55:38 -0400

    homepage done

--- movscroll_project/settings.py
+++ movscroll_project/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for movscroll_project project.
 
@@ -9,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 
 from pathlib import Path
 
@@ -116,8 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
